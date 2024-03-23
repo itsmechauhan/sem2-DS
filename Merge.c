5a4879fd423c77e9ca2741d5d82c45063604ede8
Himanshu Chauhan <103061808+itsmechauhan@users.noreply.github.com>
2024-03-23 15:41:31 +0530

    Update Merge.c

--- Merge.c
+++ Merge.c
@@ -6,25 +6,29 @@ struct node{
 	struct node* next;
 }*head1=NULL,*head2=NULL,*finalhead=NULL,*temp,*newtemp,*newnode;
 
-int count=0;
+int count=0,LLcounter=0;
 void create(struct node **);
 void traverse(struct node **);
 void merged_LL(struct node **,struct node **);
 
 int main(){
 create(&head1);
+
+create(&head2);
 printf("First LL: ");
 traverse(&head1);
-create(&head2);
 printf("\nSecond LL: ");
 traverse(&head2);
 merged_LL(&head1,&head2);
- traverse(&finalhead);
+
+printf("\nFinal LL: ");
+traverse(&finalhead);
 }
 
 void create(struct node **head){
-int i,n; 
-printf("\nEnter the size of linked list: ");
+int i,n;
+LLcounter++; 
+printf("\nEnter the size of %d linked list: ",LLcounter);
 scanf("%d",&n);
 
 for(i=1;i<=n;i++){ 
@@ -55,9 +59,10 @@ void traverse(struct node **head){
 
  void merged_LL(struct node **head1,struct node **head2){
  	temp=*head1;
- 	while(temp!=NULL){
+ 	for(int i=1;i<=count;i++){
+ 		if(temp==NULL)temp=*head2;
  		newnode=(struct node*)malloc(sizeof(struct node));
- 		temp->data=newnode->data;
+ 		newnode->data=temp->data;
  		newnode->next=NULL;
  		if(finalhead==NULL){
  			finalhead=newnode;
@@ -69,12 +74,6 @@ void traverse(struct node **head){
  		}
  		temp=temp->next;
  	}
- 	temp=*head2;
- 	while(temp!=NULL){
- 		newnode=(struct node*)malloc(sizeof(struct node));
- 		temp->data=newnode->data;
- 		newnode->next=NULL;
- 		newtemp->next=newnode;
- 		newtemp=newnode;
- }
+ 		
 }
+
